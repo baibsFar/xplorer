f859ba3a8c5ff4aa725ecdd6affefad182c1c72a
Fuji-Noobie-Ghost <fitahiana.ratia@gmail.com>
2023-01-19 21:16:52 +0300

    feat: back functionnalities

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -1,13 +1,20 @@
 import { app } from '../modules/app.module.js'
+<<<<<<< HEAD
 import { lstat } from 'fs/promises'
 import { homedir } from 'os'
 import { downloadFile, arrangeDirData } from '../service/app.service.js'
 import { username as host } from '../data/info.js'
+=======
+import { stat } from 'fs/promises'
+import { homedir } from 'os'
+import { downloadFile, arrangeDirData } from '../service/app.service.js'
+>>>>>>> 9875abf (feat: back functionnalities)
 
 function AppController() {
     app.use(async (req, res) => {
         try {
             const path = homedir() + decodeURI(req.path)
+<<<<<<< HEAD
             const stats = await lstat(path)
             if (stats.isFile()) downloadFile(res, path, stats.size)
             else if (stats.isSymbolicLink()) res.json({ message: 'Link or Symbolic Link' })
@@ -18,6 +25,13 @@ function AppController() {
         } catch (e) {
             console.error(e)
             res.json({ message: e })
+=======
+            const stats = await stat(path)
+            if (stats.isFile()) downloadFile(res, path, size)
+            else res.json(await arrangeDirData(path))
+        } catch (e) {
+            console.error(e)
+>>>>>>> 9875abf (feat: back functionnalities)
         }
     })
 }
--- src/modules/app.module.js
+++ src/modules/app.module.js
@@ -1,16 +1,24 @@
 import express from 'express'
+<<<<<<< HEAD
 import path from 'path'
 import { logActivities } from '../middlewares/app.middleware.js'
 import { HOME_DIR } from '../data/info.js'
+=======
+import { logActivities } from '../middlewares/app.middleware.js'
+>>>>>>> 9875abf (feat: back functionnalities)
 
 const app = express()
 
 app.get('/favicon.ico', (_req, res) => res.end())
+<<<<<<< HEAD
 app.set('view engine', 'ejs')
 app.set(path.join(path.resolve('../'), 'views'))
 app.use(logActivities())
 app.use(express.static('views'))
 app.use(express.static('public'))
 app.use(express.static(HOME_DIR, { dotfiles: 'allow' }))
+=======
+app.use(logActivities())
+>>>>>>> 9875abf (feat: back functionnalities)
 
 export { app }
