ebc6d87151dd5cbe1e1da9895aac606578e15364
baibs fararano <sbiab13@baibsFar>
2023-01-22 10:37:47 +0300

    feat: setting view path ; fix: symbolic link

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -1,19 +1,24 @@
 import { app } from '../modules/app.module.js'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { lstat } from 'fs/promises'
 import { homedir } from 'os'
 import { downloadFile, arrangeDirData } from '../service/app.service.js'
 import { username as host } from '../data/info.js'
 =======
 import { stat } from 'fs/promises'
+=======
+import { lstat } from 'fs/promises'
+>>>>>>> 79a04ef (feat: setting view path ; fix: symbolic link)
 import { homedir } from 'os'
 import { downloadFile, arrangeDirData } from '../service/app.service.js'
 >>>>>>> 9875abf (feat: back functionnalities)
 
 function AppController() {
-    app.use(async (req, res) => {
+    app.use(async (req, res, next) => {
         try {
             const path = homedir() + decodeURI(req.path)
+<<<<<<< HEAD
 <<<<<<< HEAD
             const stats = await lstat(path)
             if (stats.isFile()) downloadFile(res, path, stats.size)
@@ -28,11 +33,17 @@ function AppController() {
 =======
             const stats = await stat(path)
             if (stats.isFile()) downloadFile(res, path, size)
+=======
+            const stats = await lstat(path)
+            if (stats.isFile()) downloadFile(res, path, stats.size)
+            else if (stats.isSymbolicLink()) res.json({ message: 'Link or Symbolic Link' })
+>>>>>>> 79a04ef (feat: setting view path ; fix: symbolic link)
             else res.json(await arrangeDirData(path))
         } catch (e) {
             console.error(e)
 >>>>>>> 9875abf (feat: back functionnalities)
         }
+        next()
     })
 }
 
--- src/modules/app.module.js
+++ src/modules/app.module.js
@@ -1,5 +1,8 @@
 import express from 'express'
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 79a04ef (feat: setting view path ; fix: symbolic link)
 import path from 'path'
 import { logActivities } from '../middlewares/app.middleware.js'
 import { HOME_DIR } from '../data/info.js'
@@ -11,14 +14,20 @@ const app = express()
 
 app.get('/favicon.ico', (_req, res) => res.end())
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 79a04ef (feat: setting view path ; fix: symbolic link)
 app.set('view engine', 'ejs')
 app.set(path.join(path.resolve('../'), 'views'))
 app.use(logActivities())
 app.use(express.static('views'))
+<<<<<<< HEAD
 app.use(express.static('public'))
 app.use(express.static(HOME_DIR, { dotfiles: 'allow' }))
 =======
 app.use(logActivities())
 >>>>>>> 9875abf (feat: back functionnalities)
+=======
+>>>>>>> 79a04ef (feat: setting view path ; fix: symbolic link)
 
 export { app }
