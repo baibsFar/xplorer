ac339a802aa18623a89698c62b1196f4ee16447e
baibs fararano <sbiab13@baibsFar>
2023-01-22 12:15:29 +0300

    fix: adding static setup for sending file

--- src/modules/app.module.js
+++ src/modules/app.module.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import { HOME_DIR } from '../data/info.js'
 import { logActivities } from '../middlewares/app.middleware.js'
+import { HOME_DIR } from '../data/info.js'
 
 const app = express()
 
